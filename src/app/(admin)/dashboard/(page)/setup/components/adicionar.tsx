95d502dca31e0052234bcb0e82196bdb31930afc
William <williamuteich14@gmail.com>
2024-12-22 20:14:53 -0300

    Add 'Add' button and fix layout for missing data

--- src/app/(admin)/dashboard/(page)/setup/components/adicionar.tsx
+++ src/app/(admin)/dashboard/(page)/setup/components/adicionar.tsx
@@ -13,39 +13,79 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export default function ButtonAdicionar({data}: any) {
+export default function ButtonAdicionar({ data }: any) {
 
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
 
-        console.log(data);
+        const type = document.getElementById("type") as HTMLInputElement;
+        const name = document.getElementById("name") as HTMLInputElement;
+        const url = document.getElementById("url") as HTMLInputElement;
+        const value = document.getElementById("value") as HTMLInputElement;
+
+        if(!type.value || !name.value ) {
+            alert("Preencha os campos váriavel e nome!");
+            return;
+        }
+
+        const response = await fetch(`/api/setup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                type: type.value,
+                name: name.value,
+                url: url.value,
+                value: value.value
+            })
+        });
+
+        if(response.status === 201) {
+            alert("Configuração adicionada com sucesso!");
+            window.location.reload();
+        } else {
+            alert("Erro ao adicionar configuração.");
+        }
     }
 
     return (
         <div className="mb-6 w-full text-end">
             <Dialog>
                 <DialogTrigger asChild>
-                    <Button variant="outline" className="bg-green-600 text-white hover:bg-green-700 font-semibold py-1 px-4 rounded-md transition duration-300 ease-in-out">+</Button>
+                    <Button variant="outline" className="bg-green-600 text-white hover:bg-green-700 font-semibold py-1 px-4 rounded-md transition duration-300 ease-in-out">Adicionar</Button>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-[600px]">
                     <DialogHeader>
-                        <DialogTitle>Adicionar conteúdo</DialogTitle>
+                        <DialogTitle>Adicionar novo conteúdo</DialogTitle>
                         <DialogDescription>
-                            Preencha os campos abaixo para adicionar um novo valor.
+                            Preencha os campos abaixo para adicionar uma nova variável com seu respectivo nome e valor. Esses dados serão utilizados para personalizar a configuração do seu site.
                         </DialogDescription>
                     </DialogHeader>
                     <div className="grid gap-4 py-4">
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="question" className="text-right">
-                                nome
+                            <Label htmlFor="type" className="text-right">
+                                Variável
                             </Label>
-                            <Input id="question" placeholder="Digite a pergunta" className="col-span-3" />
+                            <Input id="type" placeholder="Digite o nome da variável" className="col-span-3" />
                         </div>
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="response" className="text-right">
+                            <Label htmlFor="name" className="text-right">
+                                nome
+                            </Label>
+                            <Input id="name" placeholder="Digite o nome" className="col-span-3" />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="url" className="text-right">
                                 valor
                             </Label>
-                            <Input id="response" placeholder="Digite a resposta" className="col-span-3" />
+                            <Input id="url" placeholder="Digite a url" className="col-span-3" />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="value" className="text-right">
+                                valor
+                            </Label>
+                            <Input id="value" placeholder="Digite o valor" className="col-span-3" />
                         </div>
                     </div>
                     <DialogFooter>
--- src/app/(admin)/dashboard/(page)/setup/page.tsx
+++ src/app/(admin)/dashboard/(page)/setup/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import ButtonAdicionar from "./components/adicionar";
+import Link from "next/link";
 
 export default async function Settings() {
     const response = await fetch(`${process.env.NEXTAUTH_URL}/api/setup`);
@@ -11,88 +12,72 @@ export default async function Settings() {
 
     const setup = await response.json();
 
-    const socialMedia = setup.socialMedia;
-    const contacts = setup.contacts;
+    if (setup.length === 0 || !setup) {
+        return (
+            <div className="w-full px-8 py-10 min-h-screen bg-gray-50">
+                <div className="mx-auto bg-white p-8 rounded-lg shadow-lg text-center">
+                    <h2 className="text-3xl font-semibold mb-4 text-gray-800">Nenhuma Configuração Encontrada</h2>
+                    <p className="text-gray-600 mb-6 text-sm leading-[1.6]">
+                        Ainda não há configurações para o seu site. Você pode adicionar novas variáveis para personalizar o site de acordo com suas necessidades.
+                    </p>
+                    <ButtonAdicionar data={setup} />
+                </div>
+            </div>
+        );
+    }
 
     return (
-        <div className="w-full px-6 py-8 min-h-screen">
-            <h1 className="text-4xl font-bold text-center mb-8">Página de Configurações</h1>
-
-            <div className="bg-white p-6 border rounded-lg shadow-lg mb-8">
-                <h2 className="text-3xl font-semibold mb-4">Redes Sociais</h2>
-                <p className="text-gray-600 mb-6">Gerencie suas redes sociais aqui.</p>
-
-                <table className="min-w-full table-auto border-collapse">
-                    <thead>
+        <div className="w-full px-8 py-10 min-h-screen bg-gray-50">
+            <div className="mx-auto bg-white p-8 rounded-lg shadow-lg">
+                <h2 className="text-3xl font-semibold mb-3 text-gray-800">Configurações Gerais</h2>
+                <p className="text-gray-600 mb-10 text-sm leading-[1.6]">
+                    Gerencie as variáveis do seu site, como URLs e nomes. As alterações feitas aqui são aplicadas globalmente, sem necessidade de alterar o código.
+                </p>
+                <table className="min-w-full table-auto border-collapse rounded-md border border-gray-300">
+                    <thead className="bg-gray-200">
                         <tr>
-                            <th className="py-2 border-b text-left text-sm font-medium text-gray-700 w-[150px]">Nome</th>
-                            <th className="py-2 border-b text-left text-sm font-medium text-gray-700">URL</th>
+                            <th className="py-3 px-4 border border-slate-300 text-left text-sm font-medium text-gray-700 w-[180px]">Variável</th>
+                            <th className="py-3 px-4 border border-slate-300 text-left text-sm font-medium text-gray-700 w-[180px]">Nome</th>
+                            <th className="py-3 px-4 border border-slate-300 text-left text-sm font-medium text-gray-700">URL</th>
+                            <th className="py-3 px-4 border border-slate-300 text-left text-sm font-medium text-gray-700">Valor</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {socialMedia.map((social: any) => (
-                            <tr key={social.id}>
-                                <td className="py-2 border-b text-sm text-gray-700">{social.name}</td>
-                                <td className="py-2 border-b text-sm text-gray-700">
-                                    <div className="flex items-center space-x-2">
-                                        <input
-                                            type="url"
-                                            readOnly
-                                            value={social.url}
-                                            className="w-full p-2 border border-gray-300 rounded-md"
-                                        />
-                                        <Button className="bg-blue-500 text-white hover:bg-blue-600 font-semibold py-1 px-3 rounded-md transition duration-300 ease-in-out">Editar</Button>
-                                        <Button variant="outline" className="bg-red-500 text-white hover:bg-red-600 font-semibold py-[6px] px-3 rounded-md transition duration-300 ease-in-out">Excluir</Button>
+                    <tbody className="divide-y divide-gray-300">
+                        {setup.map((config: any) => (
+                            <tr key={config.id} className="hover:bg-gray-50 transition-colors">
+                                <td className="py-3 px-4 font-medium text-sm text-gray-700 border border-gray-300">{config.type}</td>
+                                <td className="py-3 px-4 font-medium text-sm text-gray-700 border border-gray-300">{config.name}</td>
+
+                                <td className="py-3 px-4 font-medium text-sm text-gray-700 border border-gray-300">
+                                    {config.url ? (
+                                        <Link href={config.url} className="text-blue-600">
+                                            {config.url}
+                                        </Link>
+                                    ) : (
+                                        <span className="text-gray-400">URL não disponível</span>
+                                    )}
+                                </td>
+
+                                <td className="py-3 px-4 font-medium text-sm text-gray-700 border border-gray-300">
+                                    <div className="flex items-center space-x-3">
+                                        <span className="w-full p-2 border border-gray-300 rounded-md text-gray-700 truncate">
+                                            {config.value ? config.value : <span className="text-gray-400">Valor não disponível</span>}
+                                        </span>
+                                        <Button className="bg-blue-500 text-white hover:bg-blue-600 font-semibold py-1 px-3 rounded-md transition duration-300 ease-in-out">
+                                            Editar
+                                        </Button>
+                                        <Button variant="outline" className="bg-red-500 text-white hover:bg-red-600 font-semibold py-[6px] px-3 rounded-md transition duration-300 ease-in-out">
+                                            Excluir
+                                        </Button>
                                     </div>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-                <div className="mt-3 flex justify-between">
-                <ButtonAdicionar data={socialMedia}/>
+                <div className="mt-5 flex justify-between">
+                    <ButtonAdicionar data={setup} />
                 </div>
-            </div>
-
-            <div className="bg-white border p-6 rounded-lg shadow-lg">
-                <h2 className="text-3xl font-semibold mb-4">Contatos</h2>
-                <p className="text-gray-600 mb-6">Gerencie seus contatos aqui.</p>
-
-                <table className="min-w-full table-auto border-collapse">
-                    <thead>
-                        <tr>
-                            <th className="py-2 border-b text-left text-sm font-medium text-gray-700 w-[150px]">Tipo</th>
-                            <th className="py-2 border-b text-left text-sm font-medium text-gray-700">Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {contacts.map((item: any) => (
-                            <tr key={item.id}>
-                                <td className="py-2 border-b text-sm text-gray-700">{item.type}</td>
-                                <td className="py-2 border-b text-sm text-gray-700">
-                                    <div className="flex items-center space-x-2">
-                                        <input
-                                            type="text"
-                                            value={item.value}
-                                            readOnly
-                                            className="w-full p-2 border border-gray-300 rounded-md"
-                                        />
-                                        <Button className="bg-blue-500 text-white hover:bg-blue-600 font-semibold py-1 px-3 rounded-md transition duration-300 ease-in-out">Editar</Button>
-                                        <Button variant="outline" className="bg-red-500 text-white hover:bg-red-600 font-semibold py-[6px] px-3 rounded-md transition duration-300 ease-in-out">Excluir</Button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-
-                <div className="mt-3 flex justify-between">
-                <ButtonAdicionar data={contacts}/>
-                </div>
-            </div>
-
-            <div className="mt-4 text-end">
-                <Button variant="outline" className="bg-green-600 text-white hover:bg-green-700 font-semibold py-2 px-6 rounded-md transition duration-300 ease-in-out">Salvar</Button>
             </div>
         </div>
     );
