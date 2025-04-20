fe8e34ba317f3951b4189b4394f6c6726759669c
William  Uteich <66705859+williamuteich@users.noreply.github.com>
2025-04-20 17:15:12 -0300

    feat: ajustando layout de fianlização de pagamento.

--- src/app/(pages)/checkouts/pagamento/component/confirmPayment.tsx
+++ src/app/(pages)/checkouts/pagamento/component/confirmPayment.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function ConfirmPayment() {
   return (
-    <div className="flex flex-col items-center justify-center p-8 bg-white rounded-lg shadow-lg h-screen">
+    <div className="flex flex-col items-center  p-8 bg-white rounded-lg h-screen">
       <CheckCircleIcon className="w-12 h-12 text-green-600 mb-4" />
-      <h2 className="text-2xl font-semibold text-green-600">¡Felicitaciones! Tu pedido ha sido confirmado</h2>
+      <h2 className="text-2xl font-semibold text-pink-700">¡Felicitaciones! Tu pedido ha sido confirmado</h2>
       <p className="mt-4 text-gray-700 text-center">
         Tu pedido está en análisis y será procesado en breve. ¡Gracias por comprar con nosotros!
       </p>
