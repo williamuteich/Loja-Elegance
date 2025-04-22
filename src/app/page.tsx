c76616aba2ebfa32e48afe767e4723b5d09de3c7
William  Uteich <66705859+williamuteich@users.noreply.github.com>
2025-04-21 23:31:31 -0300

    feat: agora vai.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import { Container } from "./components/container";
 import Produtos from "./(pages)/produtos/components/produtos";
 import { Promocao } from "./(pages)/produtos/components/promocao";
@@ -5,16 +7,33 @@ import ListAllProdutos from "./(pages)/produtos/components/listAllProdutos";
 import BannerHome from "./components/home/bannerHome";
 import { Banners } from "./components/home/carousel/banners";
 
-export default async function Home() {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/publica/product?fetchAll=true`);
-  const res = await response.json(); 
+export default function Home() {
+  const [produtos, setProdutos] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const url = "/api/publica/product?fetchAll=true";
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("Erro ao buscar produtos");
+        const res = await response.json();
+        setProdutos(res.produtos || []);
+      } catch (e) {
+        setProdutos([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProdutos();
+  }, []);
 
   return (
     <div className="text-red-800">
       <Banners/>
-      <Promocao produtos={res.produtos} /> 
+      <Promocao produtos={produtos} /> 
       <Container >
-        <Produtos produtos={res.produtos} titulo="Productos Destacados" isDestaque={true}/>
+        <Produtos produtos={produtos} titulo="Productos Destacados" isDestaque={true}/>
         <BannerHome />
         <ListAllProdutos />
       </Container>
