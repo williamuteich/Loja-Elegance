62339f7b6dd8964b5173a32ad885e2b4cb2e8926
williamuteich <williamuteich14@gmail.com>
2025-07-06 02:04:18 -0300

    ajustando robots;

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,24 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: 'Googlebot',
         allow: ['/'],
-        disallow: '/privada/',
+        disallow: [
+          '/api/privada/',
+          '/dashboard',
+          '/dashboard/',     
+          '/dashboard/*',
+          '/profile/*',      
+        ],
       },
       {
-        userAgent: ['Applebot', 'Bingbot'],
+        userAgent: '*',
         disallow: ['/'],
       },
     ],
-  sitemap: 'https://bazarelegance.com.br/sitemap.xml',
+    sitemap: 'https://bazarelegance.com.br/sitemap.xml',
   }
 }
