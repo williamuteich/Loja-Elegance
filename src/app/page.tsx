75fbb9b5c78048d3c1e78ba6c9362d446f92928c
williamuteich <williamuteich14@gmail.com>
2025-07-11 01:10:23 -0300

    ajustando home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
+import HomeSite from "@/components/home/home";
+
 export const dynamic = "force-dynamic";
 
-import { Container } from "./components/container";
-import Produtos from "./(pages)/produtos/components/produtos";
-import { Promocao } from "./(pages)/produtos/components/promocao";
-import ListAllProdutos from "./(pages)/produtos/components/listAllProdutos";
-import BannerHome from "./components/home/bannerHome";
-import CategoriesCarousel from "./components/home/CategoriesCarousel";
-import { Banners } from "./components/home/carousel/banners";
-import Reels from "./components/reels";
 
 export default async function Home() {
 
   return (
-    <div className="text-red-800">
-      <Banners/>
-      <CategoriesCarousel />
-      <Promocao />
-      <Container >
-        <Produtos titulo="Productos Destacados" isDestaque={true} />
-        <BannerHome />
-        <ListAllProdutos />
-      </Container>
-      <Reels />
-    </div>
+    <HomeSite />
   );
 }
---
+++ src/components/home/home.tsx
@@ -0,0 +1,26 @@
+"use cache"
+
+import ListAllProdutos from "@/app/(pages)/produtos/components/listAllProdutos";
+import Produtos from "@/app/(pages)/produtos/components/produtos";
+import { Promocao } from "@/app/(pages)/produtos/components/promocao";
+import { Container } from "@/app/components/container";
+import BannerHome from "@/app/components/home/bannerHome";
+import { Banners } from "@/app/components/home/carousel/banners";
+import CategoriesCarousel from "@/app/components/home/CategoriesCarousel";
+import Reels from "@/app/components/reels";
+
+export default async function HomeSite() {
+    return (
+        <div className="text-red-800">
+            <Banners />
+            <CategoriesCarousel />
+            <Promocao />
+            <Container >
+                <Produtos titulo="Productos Destacados" isDestaque={true} />
+                <BannerHome />
+                <ListAllProdutos />
+            </Container>
+            <Reels />
+        </div>
+    )
+}
