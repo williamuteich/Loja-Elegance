d52d9dc879d949421f98aea40b7d3b178e91add1
williamuteich <williamuteich14@gmail.com>
2025-07-29 01:48:29 -0300

    ajustnado manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,11 +15,6 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '192x192',
         type: 'image/png',
       },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
     ],
   }
 }
