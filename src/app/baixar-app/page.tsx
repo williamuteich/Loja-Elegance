d6d8d13ad55737f94017272607c01387fa121ab0
williamuteich <williamuteich14@gmail.com>
2025-07-29 01:56:07 -0300

    ajustando layout de baixar-app

--- src/app/baixar-app/page.tsx
+++ src/app/baixar-app/page.tsx
@@ -5,7 +5,7 @@ import InstallAppButton from "./InstallAppButton";
 
 export default function BaixarAppPage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-white to-gray-100 p-4">
+    <main className="min-h-screen flex flex-col mt-10 justify-center bg-gradient-to-b from-white to-gray-100 p-4">
       <div className="bg-white rounded-2xl shadow-2xl max-w-md w-full p-8 flex flex-col items-center border border-gray-200">
         <Image src="/favicon-16x16.png.jpg" alt="Logo Loja Elegance" width={64} height={64} className="mb-4 rounded-full shadow" />
         <h1 className="text-2xl font-bold text-gray-800 mb-2 text-center">Leve a Loja Elegance com você!</h1>
